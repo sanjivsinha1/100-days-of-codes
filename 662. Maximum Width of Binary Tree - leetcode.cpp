c379cc6a3548cee2c8ad13d64aa64a2558e54805
sanjivsinha1 <74522768+sanjivsinha1@users.noreply.github.com>
2023-08-12 22:56:04 +0530

    Update 662. Maximum Width of Binary Tree - leetcode.cpp

--- 662. Maximum Width of Binary Tree - leetcode.cpp
+++ 662. Maximum Width of Binary Tree - leetcode.cpp
@@ -12,29 +12,30 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if(!root)
-            return 0;
-        int ans=0;
-        queue<pair<TreeNode*, int>> q;
-        q.push({root, 0});
+        if(root == NULL) return 0;
+        int ans = 0;
+        queue<pair<TreeNode*,int>>q;
+        q.push({root,0});
+
         while(!q.empty()){
-            int size = q.size();
-            int mmin = q.front().second;    // to take the id starting from zero
-            int first, last;
-            for(int i=0; i<size; i++){
-                long long cur_id = q.front().second-mmin;
-                TreeNode * node = q.front().first;
+            int size=q.size();
+            int min=q.front().second;
+            int first,last;
+            for(int i=0;i<size;i++){
+                int curid=q.front().second-min;
+                TreeNode* temp=q.front().first;
                 q.pop();
-                if(i==0) first = cur_id;
-                if(i==size-1) last = cur_id;
-                if(node->left)
-                    q.push({node->left, cur_id*2+1});
-                if(node->right)
-                    q.push({node->right, cur_id*2+2});
+
+                if(i==0) first=curid;
+                if( i== size-1) last=curid;
+
+                if(temp->left)
+                    q.push({temp->left,curid*2+1});
+                if(temp->right)
+                    q.push({temp->right,curid*2+2});
             }
-            ans = max(ans, last-first+1);
+            ans=max(ans,last-first+1);
         }
         return ans;
-        
     }
 };
